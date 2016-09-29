3274b8c4266b768698442afa9020b37d81f1fb41
Dmitriy Ovchinnikov <ovchinnikov.dk@phystech.edu>
2016-09-29 12:22:21 +0300

    hw#1 portion of pixels

--- hw#1/autocontrast.py
+++ hw#1/autocontrast.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 from sys import argv
 import os.path
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -10,23 +9,24 @@ def autocontrast(src_path, dst_path, white_perc, black_perc):
     img = cv2.imread(src_path)
     #cv2.imshow("start", img)
     b,g,r = cv2.split(img)
-    maxB = np.max(b)
-    maxG = np.max(g)
-    maxR = np.max(r)
-    minB = np.min(b)
-    minG = np.min(g)
-    minR = np.min(r)
+    maxB, maxG, maxR, minB, minG, minR = np.max(b), np.max(g), np.max(r), np.min(b), np.min(g), np.min(r)
+    b[b > maxB - maxB * float(white_perc)] = 255;
+    b[b <= minB + float(black_perc) * (maxB - minB)] = 0
+
+    g[g > maxG - maxG * float(white_perc)] = 255
+    g[g <= minG + float(black_perc) * (maxG - minG)] = 0
+
+    r[r > maxR - maxR * float(white_perc)] = 255
+    r[r <= minR + float(black_perc) * (maxR - minR)] = 0
+
     b -= minB
     g -= minG
     r -= minR
     b *= np.uint8(255.0 / (maxB - minB))
     g *= np.uint8(255.0 / (maxG - minG))
     r *= np.uint8(255.0 / (maxR - minR))
+
     result = cv2.merge((b, g, r))
-    result[result > 255] = 255.0
-    result[result < 0] = 0
-    result[result > 255 - float(white_perc) * 255] = 255
-    result[result < float(black_perc) * 255] = 0
     result.astype(np.uint8)
     #cv2.imshow("result", result)
     #cv2.waitKey(0)
